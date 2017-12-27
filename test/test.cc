66606013e2e2dc64d63dcfbb8036a48af578526e
frank <625727796@qq.com>
2017-12-27 19:03:04 +0800

    add roundtrip tests

--- test/test.cc
+++ test/test.cc
@@ -5,6 +5,8 @@
 #include <gtest/gtest.h>
 
 #include "Document.h"
+#include "StringWriteStream.h"
+#include "Writer.h"
 
 using namespace tjson;
 
@@ -59,6 +61,18 @@ using namespace tjson;
     EXPECT_EQ(doc.getType(), TYPE_STRING); \
     EXPECT_EQ(doc.getString(), str); \
 } while(false)
+
+#define TEST_ROUNDTRIP(json) do { \
+    Document doc; \
+    ParseError err = doc.parse(json); \
+    EXPECT_EQ(err, PARSE_OK); \
+    StringWriteStream os; \
+    Writer writer(os); \
+    writeTo(doc, writer); \
+    EXPECT_EQ(json, os.get()); \
+} while(false)
+
+
 
 TEST(json_error, expect_value) {
     ParseError err = PARSE_EXPECT_VALUE;
@@ -224,6 +238,7 @@ TEST(json_value, string_)
     TEST_STRING("\""sv, "\"\\\"\"");
     TEST_STRING("/\b\f\n\r\t"sv, "\"\\/\\b\\f\\n\\r\\t\"");
     TEST_STRING("蛤\0蛤\0蛤"sv, "\"蛤\\u0000蛤\\u0000蛤\"");
+    TEST_STRING("蛤\0蛤\0蛤"sv, "\"蛤\0蛤\0蛤\""sv);
     TEST_STRING("\0"sv, "\"\\u0000\"");
     TEST_STRING("\x24"sv, "\"\\u0024\"");         			/* Dollar $ */
     TEST_STRING("\xC2\xA2"sv, "\"\\u00A2\"");     			/* Cents ¢ */
@@ -342,6 +357,51 @@ TEST(json_value, object)
     EXPECT_EQ(obj["1"].getInt32(), 1);
     EXPECT_EQ(obj["2"].getInt32(), 2);
     EXPECT_EQ(obj["3"].getInt32(), 3);
+}
+
+TEST(json_round, number)
+{
+    TEST_ROUNDTRIP("0");
+    TEST_ROUNDTRIP("1");
+    TEST_ROUNDTRIP("-1");
+    TEST_ROUNDTRIP("10086.9527");
+    TEST_ROUNDTRIP("2.345e+100");
+    TEST_ROUNDTRIP("-1.11e-10");
+
+/* https://en.wikipedia.org/wiki/Double-precision_floating-point_format */
+    TEST_ROUNDTRIP("1.0000000000000002");
+    TEST_ROUNDTRIP("-1.0000000000000002");
+    // fixme: stod has bug...
+//    TEST_ROUNDTRIP("4.9406564584124654e-324");
+//    TEST_ROUNDTRIP("-4.9406564584124654e-324");
+//    TEST_ROUNDTRIP("2.2250738585072009e-308");
+//    TEST_ROUNDTRIP("-2.2250738585072009e-308");
+    TEST_ROUNDTRIP("2.2250738585072014e-308");
+    TEST_ROUNDTRIP("-2.2250738585072014e-308");
+    TEST_ROUNDTRIP("1.7976931348623157e+308");
+    TEST_ROUNDTRIP("-1.7976931348623157e+308");
+}
+
+TEST(json_round, string)
+{
+    TEST_ROUNDTRIP("\"蛤蛤蛤\"");
+    TEST_ROUNDTRIP("\"\"");
+    TEST_ROUNDTRIP("\"Hello\"");
+    TEST_ROUNDTRIP("\"Hello\\nWorld\"");
+    TEST_ROUNDTRIP("\"\\\" \\\\ / \\b \\f \\n \\r \\t\"");
+    TEST_ROUNDTRIP("\"Hello\\u0000World\"");
+}
+
+TEST(json_round, array)
+{
+    TEST_ROUNDTRIP("[]");
+    TEST_ROUNDTRIP("[null,false,true,123,\"abc\",[1,2,3]]");
+}
+
+TEST(json_round, object)
+{
+    TEST_ROUNDTRIP("{}");
+    TEST_ROUNDTRIP("{\"n\":null,\"f\":false,\"t\":true,\"i\":123,\"s\":\"abc\",\"a\":[1,2,3],\"o\":{\"1\":1,\"2\":2,\"3\":3}}");
 }
 
 int main(int argc, char **argv)
