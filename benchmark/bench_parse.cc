19b688b783be5df815d03a4e75c880499454753b
frank <625727796@qq.com>
2017-12-27 22:04:18 +0800

    benchmark file

--- benchmark/bench_parse.cc
+++ benchmark/bench_parse.cc
@@ -8,6 +8,7 @@
 #include <jackson/FileReadStream.h>
 #include <jackson/StringWriteStream.h>
 #include <jackson/Writer.h>
+#include <jackson/PrettyWriter.h>
 
 template <class ...ExtraArgs>
 void BM_parse(benchmark::State &s, ExtraArgs&&... extra_args)
@@ -18,18 +19,19 @@ void BM_parse(benchmark::State &s, ExtraArgs&&... extra_args)
             exit(1);
         json::Document doc;
         json::FileReadStream is(input);
+        fclose(input);
         if (doc.parseStream(is) != json::PARSE_OK) {
             exit(1);
         }
-        fclose(input);
         json::StringWriteStream os;
-        json::Writer writer(os);
+        json::PrettyWriter writer(os);
         json::writeTo(doc, writer);
         std::string_view ret = os.get();
         benchmark::DoNotOptimize(ret);
     }
 }
 
-BENCHMARK_CAPTURE(BM_parse, parse, "canada.json")->Unit(benchmark::kMillisecond);
+BENCHMARK_CAPTURE(BM_parse, many_double, "canada.json")->Unit(benchmark::kMillisecond);
+BENCHMARK_CAPTURE(BM_parse, many_int32, "citm_catalog.json")->Unit(benchmark::kMillisecond);
 
 BENCHMARK_MAIN()
